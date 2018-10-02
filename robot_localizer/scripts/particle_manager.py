c28150cc6945e69cf84d32af72dbdf024424a591
CleanestMink126 <gns126@gmail.com>
2018-10-02 13:39:53 -0400

    fixed comma

--- robot_localizer/scripts/particle_manager.py
+++ robot_localizer/scripts/particle_manager.py
@@ -42,7 +42,7 @@ class ParticleManager:
 		"""
 		yaws = np.expand_dims(np.random.normal(xy_yaw[2], math.pi/6.0, self.max_particles))
 		xs = np.expand_dims(np.random.normal(xy_yaw[0], .75, self.max_particles))
-		ys = np.expand_dims(np.random.normal(xy_yaw[1], ,75, self.max_particles))
+		ys = np.expand_dims(np.random.normal(xy_yaw[1], .75, self.max_particles))
 		weights = np.expand_dims(np.zeros_like(locations))
         self.current_particles = np.concatentate([xs, ys, yaws, weights],axis = 1)
 
@@ -52,7 +52,7 @@ class ParticleManager:
 		Debugging functions, prints map location, heading, and weighting for
 		each particle
 		"""
-		
+
 		for particle in current_particles:
 			print("X: %f, Y: %f, Theta: %f, Weight: %f" %(particle[0], particle[1], particle[2], particle[3]))
 
@@ -71,7 +71,7 @@ class ParticleManager:
 
 			yaws = np.expand_dims(np.random.normal(paricle[2], math.pi/6.0, self.num_mult))
 			xs = np.expand_dims(np.random.normal(paricle[0], .75, self.max_particles))
-    		ys = np.expand_dims(np.random.normal(paricle[1], ,75, self.max_particles))
+    		ys = np.expand_dims(np.random.normal(paricle[1], .75, self.max_particles))
 		    weights = np.expand_dims(np.zeros_like(locations))
 		    new_particles.append(np.concatentate([xs, ys, yaws, weights],axis = 1))
         new_particles = np.concatentate(new_particles, axis = 0)
@@ -102,4 +102,7 @@ class ParticleManager:
             p_closest = OccupancyField.get_closest_obstacle_distance(x, y)
             self.current_particles[index, 3] = p_closest
 
-
+if __name__ == "__main__":
+    pm = ParticleManager
+    pm.initParticles((0,0,0))
+    print(pm.current_particles)
